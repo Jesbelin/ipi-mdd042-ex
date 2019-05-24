0341e4f48fd30f2c366483ca9887b7c86b6f4fd3
Jesbelin <j.esbelin@gmail.com>
2019-05-24 16:42:00 +0200

    Ràf: parcourt liste csv

--- src/main/java/com/ipiecoles/java/java230/MyRunner.java
+++ src/main/java/com/ipiecoles/java/java230/MyRunner.java
@@ -34,8 +34,9 @@ public class MyRunner implements CommandLineRunner {
     private static final int NB_CHAMPS_TECHNICIEN = 7;
     private static final String REGEX_MATRICULE_MANAGER = "^M[0-9]{5}$";
     private static final int NB_CHAMPS_COMMERCIAL = 7;
-    private static final String REGEX_SALAIRE = "^[0-9]+\\.[0-9]{0,2}$";
-    /*
+    private static final String REGEX_CA = "^[0-9]+(\\.[0-9]{1,2})?$";
+    private static final String REGEX_PERFORMANCE = "[0-9]+";
+    /*private static final String REGEX_SALAIRE = "^[0-9]+\\.[0-9]{0,2}$";
     ^ => début de chaîne
     [0-9] => on un chiffre entre 0 et 9 (compris)
     + => on veut au moins 1 chiffre
@@ -43,6 +44,7 @@ public class MyRunner implements CommandLineRunner {
     {0,2} => on veut de 0 à 2 chiffres après la virgule
     $ => fin de chaîne
      */
+
 
     @Autowired
     private EmployeRepository employeRepository;
@@ -78,7 +80,7 @@ public class MyRunner implements CommandLineRunner {
         Stream<String> stream;
         //Stream contient l'ensemble des lignes sous forme de liste et il va falloir parcourir cette liste
 
-        logger.info("lecture du fichier : " + fileName);
+        logger.info("Lecture du fichier : " + fileName);
 
         try {
             stream = Files.lines(Paths.get(new ClassPathResource(fileName).getURI()));
@@ -135,14 +137,29 @@ public class MyRunner implements CommandLineRunner {
      * @throws BatchException s'il y a un problème sur cette ligne
      */
     private void processCommercial(String ligneCommercial) throws BatchException {
-        //on doit contrôler le matricule
+        Commercial c = new Commercial();
         String[] commercialFields = ligneCommercial.split(",");
-        //on split la lignes pour séparer chaque colonne. La séparation se fait sur la virgule
+        //on split la ligne pour séparer chaque colonne. La séparation se fait sur la virgule
 
         //on vérifie si le matricule (1ère colonne, donc '0') respecte bien l'expression régulière d'un matricule
-        Commercial c = new Commercial();
-        employes.add(c);
+        if (commercialFields.length != NB_CHAMPS_COMMERCIAL){ //Si la longueur du champs Commercial est différente
+            throw new BatchException("La ligne commercial ne contient pas 7 éléments mais " + commercialFields.length);
+        }
 
+        //appel de la méthode par défaut qui vérifie les formats de certain champs
+        processEmploye(ligneCommercial, c);
+
+        //vérification format chiffre d'affaire
+        if (!commercialFields[5].matches(REGEX_CA)){
+            throw new BatchException("Le chiffre d'affaire du commercial est incorrect: " + commercialFields[5]);
+        }
+
+        //vérification format performance
+        if (!commercialFields[6].matches(REGEX_PERFORMANCE)){
+            throw new BatchException( ("La performance du commercial est incorrecte : " + commercialFields[6]));
+        }
+
+        employes.add(c);
     }
 
     /**
@@ -151,40 +168,17 @@ public class MyRunner implements CommandLineRunner {
      * @throws BatchException s'il y a un problème sur cette ligne
      */
     private void processManager(String ligneManager) throws BatchException {
+        Manager m = new Manager();
         String[] managerFields = ligneManager.split(",");
 
         if (managerFields.length != NB_CHAMPS_MANAGER){
-            throw new BatchException("Le nombre de champs est incorrect");
+            throw new BatchException("La ligne manager ne contient pas 5 éléments mais " + managerFields.length);
         }
 
-        if (!managerFields[0].matches(REGEX_MATRICULE)){
-            throw new BatchException("Le matricule ne respecte pas les règles");
-        }
+        //appel de la méthode par défaut qui vérifie les formats de certain champs
+        processEmploye(ligneManager, m);
 
-        if (!managerFields[1].matches(REGEX_NOM)){
-            throw new BatchException("Le nom ne respecte pas les règles");
-        }
 
-        if (!managerFields[2].matches(REGEX_PRENOM)){
-            throw new BatchException("Le prénom ne respecte pas les règles");
-        }
-
-        try{
-            LocalDate d = DateTimeFormat.forPattern("dd/MM/yyyy").parseLocalDate(managerFields[3]);
-        }
-        catch (Exception e) {
-
-            throw new BatchException( managerFields[3] + " ne respecte pas le format de date dd/MM/yyyy");
-        }
-
-        try {
-            float salary = Float.parseFloat(managerFields[4]);
-        }
-        catch (Exception e){
-            throw new BatchException( managerFields[4] + " n'est pas un format valide de salaire");
-        }
-
-        Manager m = new Manager();
         employes.add(m);
 
     }
@@ -195,10 +189,107 @@ public class MyRunner implements CommandLineRunner {
      * @throws BatchException s'il y a un problème sur cette ligne
      */
     private void processTechnicien(String ligneTechnicien) throws BatchException {
+        Technicien t = new Technicien();
         String[] technicienFields = ligneTechnicien.split(",");
 
-        Technicien t = new Technicien();
+        if (technicienFields.length != NB_CHAMPS_TECHNICIEN){
+            throw new BatchException("La ligne technicien ne contient pas 7 éléments mais " + technicienFields.length);
+        }
+
+        //appel de la méthode par défaut qui vérifie les formats de certain champs
+        processEmploye(ligneTechnicien, t);
+
+        //vérification du grade du technicien
+        Integer grade;
+        try {
+            grade = Integer.parseInt(technicienFields[5]);
+        }
+        catch (Exception e) {
+            throw new BatchException(technicienFields[5] + " n'est pas un format valide de grade");
+        }
+
+        //vérification que le grade soit bien entre 1 et 5
+        try {
+            t.setGrade(grade);
+        }
+        catch (Exception e){
+            throw new BatchException("Le grade doit être compris entre 1 et 5");
+        }
+
+        //vérification du matricule du manager
+        if (!technicienFields[6].matches(REGEX_MATRICULE_MANAGER)){
+            throw new BatchException("La chaîne " + technicienFields[6] + " ne respecte pas l'expression régulière " + REGEX_MATRICULE_MANAGER);
+        }
+
+        //vérification de l'existence du manager dans la BDD puis dans le fichier
+        Manager manager = managerRepository.findByMatricule(technicienFields[6]);
+
+
+        if (manager == null){
+            throw new BatchException("Le manager de matricule " + technicienFields[6] + " n'a pas été trouvé en base de données");
+        }
+
+        /* parcourir liste employe
+         * vérifier si chaque élément est un manager
+         * si c'est un manager vérifier son matricule
+         * parcourir toute la liste
+         * si matricule pas trouvé, on lance l'exception
+        */
+
+
+
         employes.add(t);
+
     }
 
+    /**
+     * Méthode générique qui vérifie les champs Matricule, Nom, Prénom, Date d'embauche et Salaire
+     * @param ligneEmploye la ligne contenant les infos de l'employé à intégrer
+     * @throws BatchException s'il y a un problème sur cette ligne
+     */
+    private void processEmploye(String ligneEmploye, Employe employe) throws BatchException {
+        //on split la lignes pour séparer chaque colonne. La séparation se fait sur la virgule
+        String[] employeFields = ligneEmploye.split(",");
+
+        //vérification format matricule
+        if (!employeFields[0].matches(REGEX_MATRICULE)){
+            throw new BatchException("La chaîne: " + employeFields[0] + " ne respecte pas l'expression régulière ^[MTC][0-9]{5}$");
+        }
+
+        //vérification format nom
+        if (!employeFields[1].matches(REGEX_NOM)){
+            throw new BatchException("Le nom ne respecte pas les règles");
+        }
+
+        //vérification format prenom
+        if (!employeFields[2].matches(REGEX_PRENOM)){
+            throw new BatchException("Le prénom ne respecte pas les règles");
+        }
+
+        //vérification format date
+        LocalDate dateEmbauche;
+        try{
+            dateEmbauche = DateTimeFormat.forPattern("dd/MM/yyyy").parseLocalDate(employeFields[3]);
+        }
+        catch (Exception e) {
+
+            throw new BatchException( employeFields[3] + " ne respecte pas le format de date dd/MM/yyyy");
+        }
+
+        //vérification format salaire
+        double salaire;
+        try {
+            salaire = Double.parseDouble(employeFields[4]);
+        }
+        catch (Exception e){
+            throw new BatchException( employeFields[4] + " n'est pas un format valide de salaire");
+        }
+
+        //création d'un nouvel employé
+        /*employe.setMatricule(employeFields[0]);
+        employe.setNom(employeFields[1]);
+        employe.setPrenom(employeFields[2]);
+        employe.setDateEmbauche(dateEmbauche);
+        employe.setSalaire(salaire);*/
+    }
 }
